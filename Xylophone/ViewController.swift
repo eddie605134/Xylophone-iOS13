a8c403d7040a89c65417d1743f62ab2d3fc7a532
eddie <eddie605134@gmailcom>
2024-10-21 14:02:34 +0800

    [feature] 練習多button綁一func

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+	override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+	@IBAction func keyPressed(_ sender: UIButton) {
+		let title = sender.currentTitle ?? "A"
+		playSound(toneTitle: title)
+		
+		sender.alpha = 0.5
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+				//Bring's sender's opacity back up to fully opaque.
+				sender.alpha = 1.0
+		}
+	}
+	
+	func playSound(toneTitle: String) {
+			guard let path = Bundle.main.path(forResource: toneTitle, ofType:"wav") else {
+					return }
+			let url = URL(fileURLWithPath: path)
 
+			do {
+					player = try AVAudioPlayer(contentsOf: url)
+					player?.play()
+					
+			} catch let error {
+					print(error.localizedDescription)
+			}
+	}
+	
     
 
 }
